e617ead56e477e2296ff055b88e706355de9a855
Jyoti <jyoti.ogennavar@xecurify.com>
2025-01-05 20:20:10 +0530

    adding favicon and component folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "../styles/homePage.module.css";
 
 
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.title}>Home</h1>
-      <p className={styles.description}>This is the home page</p>
+      <h1>Home</h1>
+      <p >This is the home page</p>
       <Image src="/image.jpg" alt="Image" width={500} height={500} priority/>
     </div>
   );
